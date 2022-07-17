7c266a8c927f3245f0e7eb799967998c818a556a
ravishankar8541 <107765864+ravishankar8541@users.noreply.github.com>
2022-07-18 00:23:05 +0530

    Add files via upload

---
+++ Q1.c
@@ -0,0 +1,14 @@
+//Write a program to print MySirG N times on the screen
+#include"stdio.h"
+int main()
+{
+    int i,n;
+    printf("Enter a number :");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        printf("MySirG");
+        printf("\n");
+    }
+    printf("\n");
+    return 0;
+}
